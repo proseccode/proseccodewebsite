3da81511243288250a6c1b85e115d6df85a111ab
Sarah Penney <sarah.penney@imagination.com>
2017-03-10 11:57:37 +0000

    ADDED: onmouseout event added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 console.log('loaded');
 
-document.getElementById('proseccoImg').addEventListener('mouseover', function(){
-  this.innerHTML = "Proseccode"
+
+this.document.getElementById('proseccoImg').addEventListener('mouseover', function(){
+    this.innerHTML = "<img src='/images/laptop-fire.jpg' />";
+});
+
+
+this.document.getElementById('proseccoImg').addEventListener('mouseout', function(){
+    this.innerHTML = "<img src='/images/prosecco.jpg' />";
 });
