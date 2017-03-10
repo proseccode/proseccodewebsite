62229f4afaacc487f9d7e9e5069735e3f34fe70d
carolinewilson <carolinecwilson3+github@gmail.com>
2017-03-10 12:07:03 +0000

    Updating path reference for images

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ console.log('loaded');
 
 
 this.document.getElementById('proseccoImg').addEventListener('mouseover', function(){
-    this.innerHTML = "<img src='/images/laptop-fire.jpg' />";
+    this.innerHTML = "<img src='./images/laptop-fire.jpg' />";
 });
 
 
 this.document.getElementById('proseccoImg').addEventListener('mouseout', function(){
-    this.innerHTML = "<img src='/images/prosecco.jpg' />";
+    this.innerHTML = "<img src='./images/prosecco.jpg' />";
 });
